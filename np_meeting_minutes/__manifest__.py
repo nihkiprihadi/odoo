ca8046ef974aedc0833450e4523cb38daf628b91
Nihki Prihadi <nihki.prihadi@wiratman.co.id>
2026-05-05 11:35:57 +0700

    prepare module for odoo apps

--- np_meeting_minutes/__manifest__.py
+++ np_meeting_minutes/__manifest__.py
@@ -3,8 +3,13 @@
     "name": "PTN Notulen Meeting",
     "version": "19.0.1.0.0",
     "summary": "Notulen meeting dengan histori perubahan per item pembahasan",
+    "description": """
+        Module untuk mencatat notulen meeting, item pembahasan,
+        PIC, deadline, status, dan histori perubahan.
+    """,
     "category": "Productivity",
     "author": "Nihki Prihadi",
+    "website": "https://github.com/nihkiprihadi/np_meeting_minutes",
     "license": "LGPL-3",
     "depends": [
         "base",
@@ -18,6 +23,9 @@
         "data/meeting_minutes_sequence.xml",
         "views/meeting_minutes_views.xml",
         "report/meeting_minutes_report.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
     ],
     "installable": True,
     "application": True,
